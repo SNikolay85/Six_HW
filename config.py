a4b24a458c899e157fe450441684b3580af83fa4
nikolay <sny_85@mail.ru>
2023-08-08 01:45:35 +0300

    test

--- config.py
+++ config.py
@@ -3,4 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-PAS_BASE = os.getenv('pas_base')
+DEBUG = os.getenv('DEBUG')
+ALLOWED_HOSTS=os.getenv('ALLOWED_HOSTS')
+DB_NAME=os.getenv('cicd')
+DB_USER=os.getenv('DB_USER')
+DB_PASSWORD=os.getenv('DB_PASSWORD')
+DB_HOST=os.getenv('DB_HOST')
+DB_PORT=os.getenv('DB_PORT')
