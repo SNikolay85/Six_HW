e21ac3627e529ca9a0b88f9d3b7103cc57ab3c88
Nikolay <sny_85@mail.ru>
2023-08-09 22:46:21 +0400

    change

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 DEBUG = os.getenv('DEBUG')
 ALLOWED_HOSTS=os.getenv('ALLOWED_HOSTS')
-DB_NAME=os.getenv('cicd')
+DB_NAME=os.getenv('DB_NAME')
 DB_USER=os.getenv('DB_USER')
 DB_PASSWORD=os.getenv('DB_PASSWORD')
 DB_HOST=os.getenv('DB_HOST')
